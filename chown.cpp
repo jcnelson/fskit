6ed0eb9b3a94a06a5f4c2ec19a76b44215d8713a
jcnelson <jcnelson@cs.princeton.edu>
2014-10-03 02:16:13 -0400

    Add chown() implementation

---
+++ chown.cpp
@@ -0,0 +1,56 @@
+/*
+   fskit: a library for creating multi-threaded in-RAM filesystems
+   Copyright (C) 2014  Jude Nelson
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "chown.h"
+#include "path.h"
+
+// change the owner of a path.
+// the file must be owned by the given user.
+// NOTE: no ingroup-checking occurs--if the caller is the owner, the new_group can be arbitrary.
+// the caller should verify that the new_group is allowed by its security model.
+// Return 0 on success, negative on error:
+// -ENOMEM if oom
+// -EPERM if the caller doesn't own the file 
+// -EACCES if the caller can't search a component of the path 
+// -ENOTDIR if the path has a parent non-directory 
+// -ENOENT if the entry doesn't exist
+int fskit_chown( struct fskit_core* core, char const* path, uint64_t user, uint64_t group, uint64_t new_user, uint64_t new_group ) {
+   
+   int err = 0;
+   
+   struct fskit_entry* fent = fskit_entry_resolve_path( core, path, user, group, true, &err );
+   if( !fent || err ) {
+      if( !err ) {
+         err = -ENOMEM;
+      }
+      return err;
+   }
+
+   // can't chown unless we own the file
+   if( fent->owner != user ) {
+      fskit_entry_unlock( fent );
+      return -EPERM;
+   }
+   
+   fent->owner = new_user;
+   fent->group = new_group;
+   
+   fskit_entry_unlock( fent );
+
+   return err;
+}
---
+++ chown.h
@@ -0,0 +1,30 @@
+/*
+   fskit: a library for creating multi-threaded in-RAM filesystems
+   Copyright (C) 2014  Jude Nelson
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _FSKIT_CHOWN_H_
+#define _FSKIT_CHOWN_H_
+
+#include "entry.h"
+
+extern "C" {
+ 
+int fskit_chown( struct fskit_core* core, char const* path, uint64_t user, uint64_t group, uint64_t new_user, uint64_t new_group );
+
+}
+
+#endif
