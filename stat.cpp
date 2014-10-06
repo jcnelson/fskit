e4413f9157bd15670a0d9440d011baf64d404c60
jcnelson <jcnelson@cs.princeton.edu>
2014-10-05 23:05:26 -0400

    Include file type in the mode bits when stating it.

--- stat.cpp
+++ stat.cpp
@@ -40,19 +40,42 @@ int fskit_stat( struct fskit_core* core, char const* fs_path, uint64_t user, uin
 }
 
 // stat an inode directly
-// fill in the stat buffer (always succeeds)
+// fill in the stat buffer
 int fskit_fstat( struct fskit_core* core, struct fskit_entry* fent, struct stat* sb ) {
+   
+   int type = 0;
+   if( fent->type == FSKIT_ENTRY_TYPE_FILE ) {
+      type = S_IFREG;
+   }
+   else if( fent->type == FSKIT_ENTRY_TYPE_DIR ) {
+      type = S_IFDIR;
+   }
+   else if( fent->type == FSKIT_ENTRY_TYPE_FIFO ) {
+      type = S_IFIFO;
+   }
+   else if( fent->type == FSKIT_ENTRY_TYPE_SOCK ) {
+      type = S_IFSOCK;
+   }
+   else if( fent->type == FSKIT_ENTRY_TYPE_BLK ) {
+      type = S_IFBLK;
+   }
+   else if( fent->type == FSKIT_ENTRY_TYPE_CHR ) {
+      type = S_IFCHR;
+   }
+   else if( fent->type == FSKIT_ENTRY_TYPE_LNK ) {
+      type = S_IFLNK;
+   }
    
    sb->st_dev = 0;
    sb->st_ino = fent->file_id;
-   sb->st_mode = fent->mode;
+   sb->st_mode = type | fent->mode;
    sb->st_nlink = fent->link_count;
    sb->st_uid = fent->owner;
    sb->st_gid = fent->group;
    sb->st_rdev = fent->dev;
    sb->st_size = fent->size;
-   sb->st_blksize = 0;
-   sb->st_blocks = 0;
+   sb->st_blksize = 512;
+   sb->st_blocks = 8;
    
    sb->st_atim.tv_sec = fent->atime_sec;
    sb->st_atim.tv_nsec = fent->atime_nsec;
