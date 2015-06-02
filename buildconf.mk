6075572b44e9d4383cced091999b7a8d830dfebe
Jude Nelson <judecn@gmail.com>
2015-06-02 17:25:37 -0400

    Significantly revamped build system; more Make-like

--- buildconf.mk
+++ buildconf.mk
@@ -1,35 +1,72 @@
+# build environment
+ROOT_DIR := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
+BUILD    ?= $(ROOT_DIR)/build
+OS ?= LINUX
+BUILD_BINDIR := $(BUILD)/bin
+BUILD_SBINDIR := $(BUILD)/sbin
+BUILD_LIBDIR := $(BUILD)/lib
+BUILD_USRBIN := $(BUILD)/usr/bin
+BUILD_USRLIB := $(BUILD)/usr/lib
+BUILD_USRSHARE := $(BUILD)/usr/share
+BUILD_PKGCONFIG := $(BUILD)/usr/share/pkgconfig
+BUILD_USRSBIN := $(BUILD)/usr/sbin
+BUILD_INCLUDEDIR := $(BUILD)/usr/include/
+BUILD_ETCDIR := $(BUILD)/etc
 
-VERSION_MAJOR	:= 1
-VERSION_MINOR	:= 0
-VERSION_PATCH	:= 1
-VERSION		:= $(VERSION_MAJOR).$(VERSION_MINOR).$(VERSION_PATCH)
+# install environment
+DESTDIR			?= /
+PREFIX         ?= /usr
+BINDIR         ?= $(DESTDIR)$(PREFIX)/bin
+SBINDIR			?= $(DESTDIR)$(PREFIX)/sbin
+LIBDIR         ?= $(DESTDIR)$(PREFIX)/lib
+INCLUDEDIR     ?= $(DESTDIR)/usr/include
+USRBINDIR		?= $(DESTDIR)/usr/bin
+USRSBINDIR		?= $(DESTDIR)/usr/sbin
+USRSHAREDIR		?= $(DESTDIR)/usr/share
+PKGCONFIGDIR   ?= $(DESTDIR)/usr/lib/pkgconfig
+ETCDIR			?= $(DESTDIR)/etc
 
-CFLAGS		?= -Wall -g -fPIC
-CXX		?= g++
-CXXFLAGS	?= $(CFLAGS)
+# libfskit 
+BUILD_LIBFSKIT := $(BUILD_USRLIB)
+BUILD_LIBFSKIT_HEADERS := $(BUILD_INCLUDEDIR)/fskit
+BUILD_LIBFSKIT_DIRS := $(BUILD_LIBFSKIT) $(BUILD_LIBFSKIT_HEADERS)
+INSTALL_LIBFSKIT := $(LIBDIR)
+INSTALL_LIBFSKIT_HEADERS := $(INCLUDEDIR)
+LIBFSKIT_MAJOR := 1
+LIBFSKIT_MINOR := 0
+LIBFSKIT_PATCH := 1
 
-PKG_CONFIG	?= pkg-config
+# libfskit_fuse
+BUILD_LIBFSKIT_FUSE := $(BUILD_USRLIB)
+BUILD_LIBFSKIT_FUSE_HEADERS := $(BUILD_INCLUDEDIR)/fskit/fuse
+BUILD_LIBFSKIT_FUSE_DIRS := $(BUILD_LIBFSKIT_FUSE) $(BUILD_LIBFSKIT_FUSE_HEADERS)
+INSTALL_LIBFSKIT_FUSE := $(LIBDIR)
+INSTALL_LIBFSKIT_FUSE_HEADERS := $(INCLUDEDIR)
+LIBFSKIT_FUSE_MAJOR := 1
+LIBFSKIT_FUSE_MINOR := 0
+LIBFSKIT_FUSE_PATCH := 1
 
-PTHREAD_LIBS	?= -lpthread -lrt
-PTHREAD_CFLAGS	?=
+# compiler
+CCFLAGS     := -Wall -std=c99 -g -fPIC -fstack-protector -fstack-protector-all -pthread -Wno-unused-variable -Wno-unused-but-set-variable
+CXXFLAGS   := -Wall -g -fPIC -fstack-protector -fstack-protector-all -pthread -Wno-unused-variable -Wno-unused-but-set-variable
+INC      := -I. -I$(ROOT_DIR) -I$(BUILD_INCLUDEDIR)
+DEFS     := -D_THREAD_SAFE -D__STDC_FORMAT_MACROS 
+LIBINC   := 
+CC       := gcc
+CPP      := g++
 
-FUSE_LIBS	?= `$(PKG_CONFIG) --libs fuse`
-FUSE_CFLAGS	?= `$(PKG_CONFIG) --cflags fuse`
+# build setup
+BUILD_DIRS   := $(sort $(BUILD_LIBFSKIT_DIRS) $(BUILD_LIBFSKIT_FUSE_DIRS)) 
 
-LN		?= ln
-LN_SF		?= $(LN) -sf
-RM		?= rm
-RM_RF		?= $(RM) -Rf
-MKDIR		?= mkdir
-MKDIR_P		?= $(MKDIR) -p
-CP		?= cp
-CP_A		?= $(CP) -a
+all:
 
-PREFIX		?= /usr
-LIBDIR		?= $(PREFIX)/lib
-DATADIR		?= $(PREFIX)/share
-INCLUDEDIR	?= $(PREFIX)/include
-PKGCONFIGDIR	?= $(LIBDIR)/pkgconfig
+build_setup: $(BUILD_DIRS)
 
-# override via env to disable FUSE
-ENABLE_FUSE	?= 1
+$(BUILD_DIRS):
+	@mkdir -p "$@"
+
+# rule to make an archive member from an object file
+(%.o): %.o ;    $(AR) cr $@ $*.o
+
+# debugging...
+print-%: ; @echo $*=$($*)
